a867632b1fb8a9ec724a989dca6226a2281de00e
Oron Nadiv <oron@nadiv.us>
2019-10-30 16:06:34 -1000

    clean-up

--- static/js/app.js
+++ static/js/app.js
@@ -1,13 +1,4 @@
-// 1. Hamburger Toggle.
-
 $(document).ready(function() {
-  $('.fa-bars').on('click', function(e) {
-    $('.icons').slideToggle()
-    e.preventDefault()
-  })
-
-  // 2. Check to see if the window is top if not then display button
-
   $(window).on('scroll', function() {
     if ($(this).scrollTop() > 100) {
       $('.scrolltop').fadeIn()
@@ -16,7 +7,6 @@ $(document).ready(function() {
     }
   })
 
-  // 3. Click event to scroll to top
   setTimeout(() => {
     $('.scrolltop').on('click', function(e) {
       $('html, body').animate(
@@ -28,42 +18,10 @@ $(document).ready(function() {
       e.preventDefault()
     })
 
-    // 4. Scroll to content JS
-
-    $('.bumps').on('click', function(e) {
-      $('html, body').animate(
-        {
-          scrollTop: $('.section-1').offset().top
-        },
-        800
-      )
-      e.preventDefault()
-    })
-
-    $('.out').on('click', function(e) {
-      $('html, body').animate(
-        {
-          scrollTop: $('.section-3').offset().top
-        },
-        800
-      )
-      e.preventDefault()
-    })
-
-    $('.day').on('click', function(e) {
-      $('html, body').animate(
-        {
-          scrollTop: $('.section-4').offset().top
-        },
-        800
-      )
-      e.preventDefault()
-    })
-
     $('.scroll-down').on('click', function(e) {
       $('html, body').animate(
         {
-          scrollTop: $('.section-1').offset().top
+          scrollTop: $('.section-3').offset().top
         },
         800
       )
